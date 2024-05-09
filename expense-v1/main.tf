c43d020ca181f201b5de11ed733891f4b59df72a
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2024-05-09 10:42:26 +0530

    expense scripts

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0198935c644f120f6"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     Name = "frontend"
   }
@@ -17,7 +17,7 @@ resource "aws_route53_record" "frontend" {
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0198935c644f120f6"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     Name = "backend"
   }
@@ -33,7 +33,7 @@ resource "aws_route53_record" "backend" {
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0198935c644f120f6"]
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
   tags = {
     Name = "mysql"
   }
